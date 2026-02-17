0d2e02ba923edd90460539509d8a80c26c5ffb95
Ella Åkesson Hummerdal <ella.belle.hummerdahl@gmail.com>
2026-02-17 14:13:18 +0100

    [Removed] the booking widget caused trouble

--- src/components/BookingSection.jsx
+++ src/components/BookingSection.jsx
@@ -1,47 +1,6 @@
 import React, { useEffect } from 'react'
 
 export default function BookingSection() {
-  useEffect(() => {
-    const script = document.createElement('script')
-    script.src = 'https://widget.simplybook.it/v2/widget/widget.js'
-    script.async = true
-    script.onload = () => {
-      // eslint-disable-next-line no-undef
-      const widget = new SimplybookWidget({
-        widget_type: 'iframe',
-        url: 'https://saragungar.simplybook.it',
-        theme: 'blur',
-        theme_settings: {
-          timeline_hide_unavailable: '1',
-          hide_past_days: '0',
-          timeline_modern_display: 'as_slots',
-          sb_base_color: '#4f1200',
-          display_item_mode: 'block',
-          body_bg_color: '#ffffff',
-          dark_font_color: '#27231c',
-          light_font_color: '#e7d9cb',
-          btn_color_1: '#e7d9cb',
-          sb_company_label_color: '#4f1200',
-          hide_img_mode: '0',
-          sb_busy: '#27231c',
-          sb_available: '#e7d9cb'
-        },
-        timeline: 'modern',
-        datepicker: 'top_calendar',
-        is_rtl: false,
-        app_config: {
-          clear_session: 0,
-          allow_switch_to_ada: 0,
-          predefined: { provider: '2', service: '2' }
-        }
-      })
-      return () => widget && widget.destroy && widget.destroy()
-    }
-    document.body.appendChild(script)
-    return () => {
-      document.body.removeChild(script)
-    }
-  }, [])
 
   return (
     <div className="page-container page-container--boka">
@@ -61,9 +20,9 @@ export default function BookingSection() {
           </p>
           <p>Har du frågor? <a href="#contact-section">Kontakta mig</a></p>
         </div>
-        <div className="booking-widget">
+        {/* <div className="booking-widget">
           <div id="simplybook-widget"></div>
-        </div>
+        </div> */}
       </section>
     </div>
   )
